a6f068a69cc5cee738e43c8679d971a0cea76c78
ingrid-benicio <cantstopbluh@gmail.com>
2023-04-27 13:04:04 -0300

    first commit

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+person = input('A famous person: ')
+day = input('A day: ')
+event = input('An event: ')
+
+
+madlib_Sasuke = f" Sasuke: I'm the only one who could destroy that {person} \n \
+Sakura: What? Who? You mean the sensei? \n \
+Sasuke: That {day} I was crying. \n \
+Sakura: When were you crying? \n \
+Sasuke: It was my {event} \n "
+
+print (madlib_Sasuke)
+
+# teste
